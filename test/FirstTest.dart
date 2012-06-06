fe0852aeffe6e9ab041107cb462ccc8dc64ccc87
Guillaume Girou <girou.g@sfeir.com>
2012-06-06 14:41:12 +0200

    First test

---
+++ test/FirstTest.dart
@@ -0,0 +1,8 @@
+//#import('../../../dart-editor/dart-sdk/lib/unittest/unittest.dart');
+#import('dart:unittest');
+main() {
+  test('this is a test', () {
+    int x = 2 + 3;
+    expect(x).equals(5);
+  });
+}
